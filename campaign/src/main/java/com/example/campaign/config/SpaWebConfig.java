f99b6e2072d27298361648e927fe97a92e47b8dc
Daniel Salawa <dsalawa8@gmail.com>
2025-07-20 15:46:43 +0200

    fallback

--- campaign/src/main/java/com/example/campaign/config/SpaWebConfig.java
+++ campaign/src/main/java/com/example/campaign/config/SpaWebConfig.java
@@ -1,9 +1,11 @@
 package com.example.campaign.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
+@Profile("!test")
 public class SpaWebConfig implements WebMvcConfigurer {
 
     @Override
